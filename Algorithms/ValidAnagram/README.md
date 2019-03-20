4619e6783e28a0dfd1fa5139ca8402a5e9b9439a
Xue <1099232082@qq.com>
2019-03-20 14:51:23 +0800

    巧妙应用方法

---
+++ Algorithms/ValidAnagram/README.md
@@ -0,0 +1,13 @@
+运用`split()`，`sort()`，可以轻松解决
+```javascript
+// 字符串转数组，再排序
+var a = s.split('').sort(), b = t.split('').sort();
+```
+```javascript
+// 数组转字符串，再比较
+return a.toString() === b.toString()
+```
+
+
+
+
---
+++ Algorithms/ValidAnagram/ValidAnagram.js
@@ -0,0 +1,27 @@
+/**
+ * Given two strings s and t , write a function to determine if t is an anagram of s.
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+ */
+
+ /**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    //先比较长度
+    if (s.length != t.length)
+        return false;
+    // 字符串转数组，再排序
+    var a = s.split('').sort(), b = t.split('').sort();
+    // 数组转字符串，再比较
+    return a.toString() === b.toString()
+};
